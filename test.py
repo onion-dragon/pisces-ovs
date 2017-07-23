3b35db8417d6ff5485d26d34d6f843e057d6cf8e
r630-2 <longlcy6@163.com>
2017-07-23 21:06:21 +0800

    说明

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 import sys,getopt
-opts,args=getopt.getopt(sys.argv[1:],"hi:o")
+opts,args=getopt.getopt(sys.argv[1:],"hi:o",["name","user","file","help"])
 input_file=""
 output_file=""
 for op,value in opts:
-  if op == "-i":
+  if op in ( "-n","--name"):
     input_file = value
-  elif op == "-o":
+  elif op in ("-u","--user"):
     output_file = value
+  elif op in ("-f","--file"):
+    output_file = value
+  elif op in ("-h","--help"):
+    print("------说明------")
+    print("-n,--name 指定虚拟交换机的名称" )
+    print("-u,--user 交换机所属用户 ")
+    print("-f,--file 交换机源文件")
+    print("-h,--help 获取帮助文件")
+
+
+"""
   elif op == "-h":
     usage()
     sys.exit()
-	  
+"""	  
