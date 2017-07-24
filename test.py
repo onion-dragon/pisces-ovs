3efa3e7315187df8480fca5b7f3873ba757e3221
r630-2 <longlcy6@163.com>
2017-07-25 00:21:24 +0800

    fail

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import sys,getopt
-opts,args=getopt.getopt(sys.argv[1:],"hi:o",["name","user","file","help"])
+opts,args=getopt.getopt(sys.argv[1],"hi",["build"]sys.argv[2:],"hi:o",["control","file","help"])
 input_file=""
 output_file=""
 for op,value in opts:
-  if op in ( "-n","--name"):
+  if op in ( "-b","--build"):
     input_file = value
+    name=args[0]
+    user=args[1]
+    path=args[2]
+    print("build pisces_switch %s from %s,user is %s"%(path,name,user))
   elif op in ("-u","--user"):
     output_file = value
   elif op in ("-f","--file"):
-    output_file = value
+    output_file = value  
   elif op in ("-h","--help"):
     print("------说明------")
-    print("-n,--name 指定虚拟交换机的名称" )
-    print("-u,--user 交换机所属用户 ")
+    print("-b,--build  编译虚拟交换机" )
+    print("-c,--control 交换管理功能 ")
     print("-f,--file 交换机源文件")
     print("-h,--help 获取帮助文件")
 
